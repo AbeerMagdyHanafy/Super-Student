dcc02e1bad9f971cbb806fe5bc2eb4e87d1e5711
nouranelsherif <nouran.a.elsherif@hotmail.com>
2016-05-17 18:17:03 +0200

    MAP is working, paths need to be updated

--- app/src/main/java/com/example/superstudent/Chat.java
+++ app/src/main/java/com/example/superstudent/Chat.java
@@ -3,6 +3,8 @@ package com.example.superstudent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,5 +28,43 @@ public class Chat extends AppCompatActivity {
 
         }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent;
+        if (id == R.id.home) {
+            intent = new Intent( Chat.this,Home_Activity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.profile) {
+            intent = new Intent(Chat.this, Profile_Activity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.to_do_list) {
+            intent = new Intent(Chat.this, ToDo_Activity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.materials) {
+            intent = new Intent(Chat.this, Materials_Activity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.map) {
+            intent = new Intent(Chat.this, Map_Activity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        else if (id == R.id.chat) {
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
